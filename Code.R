2c3fdb9e9ff7a44fa7dd5b903da1963b26f2990a
jackegriffin <jackegriffin1@gmail.com>
2025-09-09 10:19:27 +0100

    Jack added some code

--- Code.R
+++ Code.R
@@ -1,2 +1,11 @@
 # Our Code
 
+# Library
+# Install packages
+
+install.packages("dplyr")
+library(dplyr)
+install.packages("vegan")
+library(vegan)
+
+# Data has already been cleaned and sorted using Excel, and so the code presented will dive straight into some of our analysis.
